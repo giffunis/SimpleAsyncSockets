2d47ae62d7bd34b0ea22e8229a12e8643388e739
José Saúl Giffuni Becerra <giffunis@gmail.com>
2018-07-16 19:59:07 +0100

    Creating a net.core console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleAsyncSockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicación de consola que demuestra el funcionamiento de los Sockets");
+        }
+    }
+}
