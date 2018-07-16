a4ebd9b23fb002b8a11485d8e1c9c6e59976c571
José Saúl Giffuni Becerra <giffunis@gmail.com>
2018-07-16 20:10:16 +0100

    ISimpleAsyncListener (abstract class) created

---
+++ ISimpleAsyncListener.cs
@@ -0,0 +1,35 @@
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleAsyncSockets{
+    public abstract class ISimpleAsyncListener{
+
+        IPEndPoint ipEndPoint;
+        int backlog;
+
+        TcpListener listener;
+
+
+        public ISimpleAsyncListener(){
+            ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            listener = new TcpListener(ipEndPoint);
+            listener.Start();
+        }
+
+        public ISimpleAsyncListener(int port){
+            ipEndPoint = new IPEndPoint(IPAddress.Any, port);
+            listener = new TcpListener(ipEndPoint);
+            listener.Start();
+        }
+
+        public ISimpleAsyncListener(IPAddress ipAddress, int port){
+            ipEndPoint = new IPEndPoint(ipAddress, port);
+            listener = new TcpListener(ipEndPoint);
+            listener.Start();
+        }
+
+        public abstract void Listen();
+
+    }
+}
